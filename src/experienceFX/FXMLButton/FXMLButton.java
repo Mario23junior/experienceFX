a015a3a1f378d1b5378330256ad9464f5d2445e3
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-29 02:53:40 -0300

    Criando tela demonstrativa

--- src/experienceFX/FXMLButton/FXMLButton.java
+++ src/experienceFX/FXMLButton/FXMLButton.java
@@ -1,10 +1,26 @@
 package experienceFX.FXMLButton;
 
-public class FXMLButton {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class FXMLButton extends Application{
 
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/FXMLButton.fxml"));
+		
+		Scene scene = new Scene(root);
+		
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+
+	 public static void main(String[] args) {
+		launch(args);
 	}
 
 }
--- src/experienceFX/FXMLButton/FXMLButtonController.java
+++ src/experienceFX/FXMLButton/FXMLButtonController.java
@@ -1,5 +1,28 @@
 package experienceFX.FXMLButton;
 
-public class FXMLButtonController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLButtonController implements Initializable{
+
+	@FXML
+	private Label labelOlaMundo;
+	
+	@FXML
+	private void acaoDoButton(ActionEvent event) {
+		System.out.println("Ola eu fui clicado");
+		labelOlaMundo.setText("Ola mundo");
+	}
+	
+
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+ 		
+	}
 
 }
