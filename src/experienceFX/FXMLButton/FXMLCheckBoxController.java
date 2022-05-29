9105cdec53f333d46d917b09fb01a06d7d73769b
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-29 04:42:59 -0300

    Criando esqueleto de checkBox

---
+++ src/experienceFX/FXMLButton/FXMLCheckBoxController.java
@@ -0,0 +1,30 @@
+package experienceFX.FXMLButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+
+public class FXMLCheckBoxController implements Initializable {
+
+	@FXML
+	private CheckBox cdJava;
+
+	@FXML
+	private CheckBox cdSwift;
+
+	@FXML
+	void capturarValores(ActionEvent event) {
+
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
