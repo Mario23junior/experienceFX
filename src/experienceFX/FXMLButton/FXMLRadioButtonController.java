31da5ca30f2049de75fb4ee53a651b6b40b5db83
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-29 05:07:47 -0300

    Criando estrutura de radio button

---
+++ src/experienceFX/FXMLButton/FXMLRadioButtonController.java
@@ -0,0 +1,18 @@
+package experienceFX.FXMLButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FXMLRadioButtonController implements Initializable{
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
