46eaba33d50040b4cee2b6b063e6ee8492d5efed
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-05-29 03:32:59 -0300

    Criando modelo

---
+++ src/experienceFX/model/Categoria.java
@@ -0,0 +1,35 @@
+package experienceFX.model;
+
+public class Categoria {
+   
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		//TODO Auto-generated constructor stub
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
